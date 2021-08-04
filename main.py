17e26edf4cdecd15cc6fce0fe935c6ac67d69dcc
Soran Ghaderi <soran.gdr.cs@gmail.com>
2021-08-04 06:01:00 +0450

    Feat
    Implement MultiHeadAttention class
    functions:
    1. call

--- main.py
+++ main.py
@@ -118,3 +118,29 @@ class MultiHeadAttention(tf.keras.layers.Layer):
         X = tf.reshape(X, shape=(-1, self.num_heads, X.shape[1], X.shape[2]))
         X = tf.transpose(X, perm=(0, 2, 1, 3))
         return tf.reshape(X, shape=(X.shape[0], X.shape[1], -1))
+
+    def call(self, queries, values, keys, valid_lens, window_mask=None, **kwargs):
+        # Shape of queries, keys, or values:
+        # (batch_size, no. of queries or key-value pairs, num_hiddens)
+        # Shape of valid_lens: (batch_size,) or (batch_size, no. of queries)
+        # After transposing, shape of output queries, keys, or values:
+        # (batch_size * num_heads, no. of queries or key-value pairs,
+        # num_hiddens / num_heads)
+        queries = self.transpose_qkv(self.W_q(queries))
+        keys = self.transpose_qkv(self.W_k(keys))
+        values = self.transpose_qkv(self.W_v(values))
+
+        if valid_lens is not None:
+            # On axis 0, copy the first item (scalar or vector) for num_heads
+            # times, then copy the next item, and so on
+            valid_lens = tf.repeat(valid_lens, repeats=self.num_heads, axis=0)
+
+        # Shape of output: (batch_size * num_heads, no. of queries,
+        # num_hiddens / num_heads)
+        output = self.attention(
+            queries, keys, values, valid_lens, window_mask, **kwargs
+        )
+
+        # Shape of output_concat: (batch_size, no. of queries, num_hiddens)
+        output_concat = self.inverse_transpose_qkv(output)
+        return self.W_o(output_concat)
