b0ccbd8b2d7dcf991e5a3e354f5df100b98a192c
Soran Ghaderi <soran.gdr.cs@gmail.com>
2021-08-20 23:34:42 +0450

    PositionalEncoding class
    Implement __init__ method

--- main.py
+++ main.py
@@ -166,5 +166,13 @@ class MultiHeadAttention(tf.keras.layers.Layer):
 
 
 class PositionalEncoding(tf.keras.layers.Layer):
-    def __init__(self):
-        pass
+    def __init__(self, num_hiddens, dropout, max_len=1000):
+        super().__init__()
+        self.dropout = tf.keras.layers.Dropout(dropout)
+        # Create a long enough P
+        self.P = np.zeros((1, max_len, num_hiddens))
+        X = np.arange(max_len, dtype=np.float32).reshape(-1, 1) / np.power(
+            10000, np.arange(0, num_hiddens, 2, dtype=np.float32) / num_hiddens
+        )
+        self.P[:, :, 0::2] = np.sin(X)
+        self.P[:, :, 1::2] = np.cos(X)
