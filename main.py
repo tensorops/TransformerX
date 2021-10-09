e74b033711756c12ca3bbed0bce51353aeb32c5b
Soran Ghaderi <soran.gdr.cs@gmail.com>
2021-10-09 12:43:06 +0350

    TransformerEncoderBlock class:
    1. Implement call() method

--- main.py
+++ main.py
@@ -274,3 +274,7 @@ class TransformerEncoderBlock(tf.keras.layers.Layer):
         self.addnorm1 = AddNorm(norm_shape, dropout)
         self.ffn = PositionWiseFFN(ffn_num_hiddens, num_hiddens)
         self.addnorm2 = AddNorm(norm_shape, dropout)
+
+    def call(self, X, valid_lens, **kwargs):
+        Y = self.addnorm1(X, self.attention(X, X, X, valid_lens, **kwargs), **kwargs)
+        return self.addnorm2(Y, self.ffn(Y), **kwargs)
