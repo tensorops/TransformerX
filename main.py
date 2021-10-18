b0d6cd7303402466b5f0deef8e0103156b0a95d5
Soran Ghaderi <soran.gdr.cs@gmail.com>
2021-10-18 23:07:23 +0350

    TransformerEncoder class:
    1. Implement __init__() method

--- main.py
+++ main.py
@@ -282,3 +282,36 @@ class TransformerEncoderBlock(tf.keras.layers.Layer):
 
 class TransformerEncoder(tf.keras.layers.Layer):
     """Transformer encoder that encompasses one or more TransformerEncoderBlock blocks."""
+
+    def __init__(
+        self,
+        vocab_size,
+        key_size,
+        query_size,
+        value_size,
+        num_hiddens,
+        norm_shape,
+        ffn_num_hiddens,
+        num_heads,
+        num_blks,
+        dropout,
+        bias=False,
+    ):
+        super().__init__()
+        self.num_hiddens = num_hiddens
+        self.embedding = tf.keras.layers.Embedding(vocab_size, num_hiddens)
+        self.pos_encoding = d2l.PositionalEncoding(num_hiddens, dropout)
+        self.blks = [
+            TransformerEncoderBlock(
+                key_size,
+                query_size,
+                value_size,
+                num_hiddens,
+                norm_shape,
+                ffn_num_hiddens,
+                num_heads,
+                dropout,
+                bias,
+            )
+            for _ in range(num_blks)
+        ]
