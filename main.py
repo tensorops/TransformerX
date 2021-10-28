7a03a655886561b721cf951689d90587c63a9daa
Soran Ghaderi <soran.gdr.cs@gmail.com>
2021-10-28 18:12:31 +0350

    TransformerDecoderBlock class:
    1. Implement call() method

--- main.py
+++ main.py
@@ -359,3 +359,25 @@ class TransformerDecoderBlock(tf.keras.layers.Layer):
         self.addnorm2 = AddNorm(norm_shape, dropout)
         self.ffn = PositionWiseFFN(ffn_num_hiddens, num_hiddens)
         self.addnorm3 = AddNorm(norm_shape, dropout)
+
+    def call(self, X, state, **kwargs):
+        """During training, all the tokens of any output sequence are processed at the same time, so state[2][self.i]
+        is None as initialized. When decoding any output sequence token by token during prediction, state[2][self.i]
+        contains representations of the decoded output at the i-th block up to the current time step"""
+        enc_outputs, enc_valid_lens = state[0], state[1]
+        if state[2][self.i] is None:
+            key_values = X
+        else:
+            key_values = tf.concat((state[2][self.i], X), axis=1)
+        state[2][self.i] = key_values
+        if kwargs["training"]:
+            batch_size, num_steps, _ = X.shape
+            # Shape of dec_valid_lens: (batch_size, num_steps), where every
+            # row is [1, 2, ..., num_steps]
+            dec_valid_lens = tf.repeat(
+                tf.reshape(tf.range(1, num_steps + 1), shape=(-1, num_steps)),
+                repeats=batch_size,
+                axis=0,
+            )
+        else:
+            dec_valid_lens = None
