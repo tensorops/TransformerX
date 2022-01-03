303eb3db00a9b5c00fdd0f52af255257402e276d
Soran Ghaderi <soran.gdr.cs@gmail.com>
2022-01-04 02:48:23 +0350

    DataModule class:
    1. initiate the calss
    2. Add a brief docstring

--- data_loader.py
+++ data_loader.py
@@ -1,2 +1,5 @@
-class HyperParameters:
-    """Base class for hyperparameters"""
+class DataModule:
+    """Base class for data loaders"""
+
+    def __init__(self, data_directory="../data"):
+        self.data_directory = data_directory
