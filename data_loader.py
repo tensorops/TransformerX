12ee0f2006a782d97f0e863c4a0256fd18a0e1a9
Soran Ghaderi <soran.gdr.cs@gmail.com>
2022-01-08 13:00:53 +0350

    feat (DataModule class): Add train_dataloader method

--- data_loader.py
+++ data_loader.py
@@ -3,3 +3,6 @@ class DataModule:
 
     def __init__(self, data_directory="../data"):
         self.data_directory = data_directory
+
+    def train_dataloader(self):
+        return self.get_dataloader(train=True)
