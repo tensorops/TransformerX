626b58312454742526309405227033912e6afd96
Soran Ghaderi <soran.gdr.cs@gmail.com>
2022-06-11 19:17:59 +0450

    refactor: Move masked_softmax to utils.py

---
+++ utils.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+
+
+def masked_softmax(X, valid_lens):
+    """Perform softmax operation by masking elements on the last axis."""
+
+    # X: 3D tensor, valid_lens: 1D or 2D tensor
+    def _sequence_mask(X, valid_len, value=0):
+        maxlen = X.shape[1]
+        mask = tf.range(start=0, limit=maxlen, dtype=tf.float32)[None, :] < tf.cast(
+            valid_len[:, None], dtype=tf.float32
+        )
+
+        if len(X.shape) == 3:
+            return tf.where(tf.expand_dims(mask, axis=-1), X, value)
+        else:
+            return tf.where(mask, X, value)
+
+    if valid_lens is None:
+        return tf.nn.softmax(X, axis=-1)
+    else:
+        shape = X.shape
+        if len(valid_lens.shape) == 1:
+            valid_lens = tf.repeat(valid_lens, repeats=shape[1])
+
+        else:
+            valid_lens = tf.reshape(valid_lens, shape=-1)
+        # On the last axis, replace masked elements with a very large negative
+        # value, whose exponentiation outputs 0
+        X = _sequence_mask(tf.reshape(X, shape=(-1, shape[-1])), valid_lens, value=-1e6)
+        return tf.nn.softmax(tf.reshape(X, shape=shape), axis=-1)
