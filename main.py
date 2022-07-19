700b455bfb874edb027f090e55e7bcb7dff186d7
Soran Ghaderi <soran.gdr.cs@gmail.com>
2022-07-19 13:19:01 +0450

    refactor: Move Module class to base.py module

--- main.py
+++ main.py
@@ -5,42 +5,7 @@ from data_loader import MTFraEng
 # os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 from layers.transformer_decoder import TransformerDecoder
 from layers.transformer_encoder import TransformerEncoder
-
-
-class Module(tf.keras.Model):
-    """Base class for models"""
-
-    def __init__(self, plot_train_per_epoch=2, plot_valid_per_epoch=1):
-        super().__init__()
-        # self.save_hyperparameters()
-        self.training = None
-        self.plot_train_per_epoch = plot_train_per_epoch
-        self.plot_valid_per_epoch = plot_valid_per_epoch
-
-    def loss(self, y_hat, y):
-        raise NotImplementedError
-
-    def forward(self, X):
-        assert hasattr(self, "net"), "Neural network is defined"
-        return self.net(X)
-
-    def call(self, X, *args, **kwargs):
-        if kwargs and "training" in kwargs:
-            self.training = kwargs["training"]
-        return self.forward(X, *args)
-
-    def training_step(self, batch):
-        l = self.loss(self(*batch[:-1]), batch[-1])
-        # self.plot("loss", l, train=True)
-        return l
-
-    def validation_step(self, batch):
-        l = self.loss(self(*batch[:-1]), batch[-1])
-        self.plot("loss", l, train=False)
-
-    def configure_optimizers(self):
-        """Return optimizer"""
-        return tf.keras.optimizers.SGD(self.lr)
+from training.base import Module
 
 
 class Classifier(Module):
