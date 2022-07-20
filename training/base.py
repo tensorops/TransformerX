3f9a678caed2e561e0d9c3d9ef99d2e7f3ba13af
Soran Ghaderi <soran.gdr.cs@gmail.com>
2022-07-21 00:31:52 +0450

    refactor: Move Classifier class to base.py module

--- training/base.py
+++ training/base.py
@@ -35,3 +35,35 @@ class Module(tf.keras.Model):
     def configure_optimizers(self):
         """Return optimizer"""
         return tf.keras.optimizers.SGD(self.lr)
+
+
+class Classifier(Module):
+    """Classifier class"""
+
+    def validation_step(self, batch):
+        Y_hat = self(*batch[:-1])
+        self.plot("loss", self.loss(Y_hat, batch[-1]), train=False)
+        self.plot("acc", self.accuracy(Y_hat, batch[-1]), train=False)
+
+    @staticmethod
+    def accuracy(Y_hat, Y, averaged=True):
+        """Compute the number of correct predictions"""
+        Y_hat = tf.reshape(Y_hat, (-1, Y_hat.shape[-1]))
+        preds = tf.astype(tf.argmax(Y_hat, axis=1), Y.dtype)
+        compare = tf.astype(preds == tf.reshape(Y, -1), tf.float32)
+        return tf.reduce_mean(compare) if averaged else compare
+
+    @staticmethod
+    def loss(Y_hat, Y, averaged=True):
+        """Compute loss"""
+        Y_hat = tf.reshape(Y_hat, (-1, Y_hat.shape[-1]))
+        Y = tf.reshape(Y, (-1,))
+        fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+        return fn(Y, Y_hat)
+
+    def layer_summary(self, X_shape):
+        """Layer summary"""
+        X = tf.random.normal(X_shape)
+        for layer in self.net.layers:
+            X = layer(X)
+            print(layer.__class__.__name__, "output shape:\t", X.shape)
