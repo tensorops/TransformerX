227c246a7a13d7c8ae633fdb3f3fe4073ce122fa
Soran Ghaderi <soran.gdr.cs@gmail.com>
2022-12-07 06:12:38 +0330

    doc: Add docs for the transformer_decoder.py
    both for the class and the call method

--- transformerx/layers/transformer_decoder.py
+++ transformerx/layers/transformer_decoder.py
@@ -5,7 +5,48 @@ from transformerx.layers.transformer_decoder_block import TransformerDecoderBloc
 
 
 class TransformerDecoder(tf.keras.layers.Layer):
-    """Transformer decoder that encompasses one or more TransformerDecoderBlock blocks."""
+    """Transformer decoder that encompasses one or more TransformerDecoderBlock blocks.
+
+    Transformer decoder that encompasses one or more TransformerDecoderBlock blocks.
+
+    The TransformerDecoder processes the input sequences and produces the output sequences.
+    It also generates attention weights for each of the two attention layers in each of the
+    TransformerDecoderBlock blocks.
+
+    Parameters
+    ----------
+    vocab_size : int
+        Vocabulary size.
+    depth : int
+        Dimension of each input sequence.
+    norm_shape : str
+        Shape of the normalization layer.
+    ffn_num_hiddens : int
+        Number of hidden units in the feed-forward network.
+    num_heads : int
+        Number of attention heads.
+    n_blocks : int
+        Number of encoder blocks.
+    dropout : float
+        Dropout rate.
+
+    Attributes
+    ----------
+    depth : int
+        The depth (i.e., number of channels) of the input and output representations.
+    n_blocks : int
+        The number of TransformerDecoderBlock blocks in the decoder.
+    embedding : tf.keras.layers.Embedding
+        The embedding layer that maps the input sequences to their input representations.
+    pos_encoding : AbsolutePositionalEncoding
+        The absolute positional encoding layer that adds position information to the input
+        representations.
+    blocks : List[TransformerDecoderBlock]
+        The list of TransformerDecoderBlock blocks that process the input sequences.
+    dense : tf.keras.layers.Dense
+        The dense layer that maps the output representations to the output sequences.
+    """
+
 
     def __init__(
             self,
@@ -39,6 +80,53 @@ class TransformerDecoder(tf.keras.layers.Layer):
         return [enc_outputs, enc_valid_lens, [None] * self.n_blocks]
 
     def call(self, X, state, **kwargs):
+        """Forward call of the Transformer decoder.
+
+        Parameters
+        ----------
+        X : tf.Tensor
+            Input tensor with shape (batch_size, no. of queries or key-value pairs, depth).
+        state : List
+            List of decoder state tensors.
+        **kwargs
+            Other keyword arguments.
+
+        Returns
+        -------
+        output : tf.Tensor
+            Output tensor with shape (batch_size, no. of queries or key-value pairs, depth).
+        state : List
+            Updated list of decoder state tensors.
+
+        Examples
+        --------
+        >>> # Initialize a TransformerDecoder with a vocabulary size of 1000 and a depth of 512
+        >>> decoder = TransformerDecoder(
+        ...    vocab_size=1000,
+        ...    depth=512,
+        ...    norm_shape=(512,),
+        ...    n_blocks=6,
+        ...    dropout=0.2
+        >>> )
+
+        >>> # Define the input sequence, with batch size of 2 and sequence length of 10
+        >>> inputs = tf.random.uniform((2, 10), minval=0, maxval=1000, dtype=tf.int32)
+
+        >>> # Define the initial state of the decoder, which should be the output from the encoder
+        >>> # and the valid sequence lengths from the encoder
+        >>> enc_outputs = tf.random.normal((2, 10, 512))
+        >>> enc_valid_lens = tf.constant([10, 5], dtype=tf.int32)
+        >>> initial_state = decoder.init_state(enc_outputs, enc_valid_lens)
+
+        >>> # Call the decoder to get the output and the updated state
+        >>> output, state = decoder(inputs, initial_state)
+
+        >>> # The output will be a tensor of shape (2, 10, 1000) containing the predicted
+        >>> # probabilities for each of the input tokens at each timestep
+        >>> # The state will be a tuple containing the encoder outputs, the encoder valid lengths,
+        >>> # and a list of attention weights for each block in the decoder
+        """
+
         X = self.pos_encoding(
                 self.embedding(X) * tf.math.sqrt(tf.cast(self.depth, dtype=tf.float32)),
                 **kwargs,
