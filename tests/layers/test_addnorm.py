33f3206d81e3ac3983123edd114064cd72538094
Soran Ghaderi <soran.gdr.cs@gmail.com>
2022-12-14 02:48:35 +0330

    test: call method test case

--- tests/layers/test_addnorm.py
+++ tests/layers/test_addnorm.py
@@ -15,6 +15,30 @@ class TestAddNorm:
         assert addnorm.norm_shape == norm_shape
         assert addnorm.dropout_rate == dropout_rate
 
+    def test_call(self):
+        def test_call():
+            norm_shape = [0, 1]
+            dropout_rate = 0.2
+            addnorm = AddNorm(norm_shape, dropout_rate)
+
+            x = tf.constant(np.arange(10).reshape(5, 2) * 10, dtype=tf.float32)
+            y = tf.constant(np.arange(10).reshape(5, 2) * 10, dtype=tf.float32)
+
+            # Test the shape of the output tensor
+            assert addnorm(x, y).shape == (5, 2)
+
+            # Test the output tensor values
+            expected_output = np.array(
+                [
+                    [-1.5666986, -1.2185433],
+                    [-0.8703881, -0.52223283],
+                    [-0.17407762, 0.17407762],
+                    [0.52223283, 0.8703881],
+                    [1.2185433, 1.5666986],
+                ]
+            )
+            np.testing.assert_almost_equal(addnorm(x, y), expected_output, decimal=4)
+
     def test_invalid_dropout_rate(self):
         # Test that a ValueError is raised if an invalid dropout rate is provided
         norm_shape = (1, 2)
