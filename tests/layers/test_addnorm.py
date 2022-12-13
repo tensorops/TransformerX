45aabc5fea2baad4e3749b67c5c60b979532f727
Soran Ghaderi <soran.gdr.cs@gmail.com>
2022-12-14 02:48:08 +0330

    test: invalid_dropout_rate test case

--- tests/layers/test_addnorm.py
+++ tests/layers/test_addnorm.py
@@ -15,4 +15,10 @@ class TestAddNorm:
         assert addnorm.norm_shape == norm_shape
         assert addnorm.dropout_rate == dropout_rate
 
+    def test_invalid_dropout_rate(self):
+        # Test that a ValueError is raised if an invalid dropout rate is provided
+        norm_shape = (1, 2)
+        dropout_rate = -0.2  # This should be between 0 and 1
 
+        with pytest.raises(ValueError):
+            addnorm = AddNorm(norm_shape, dropout_rate)
