b0375ac449b1c8b7025ded6edb7d23f6a0aa9fea
Soran Ghaderi <soran.gdr.cs@gmail.com>
2022-12-14 03:40:42 +0330

    test: dotproduct
    setup
    test_output_shape

---
+++ tests/layers/test_dot_product_attention.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pytest
+import tensorflow as tf
+
+from transformerx.utils import masked_softmax
+from transformerx.layers import DotProductAttention
+
+
+class TestDotProductAttention:
+    # Set up the test class with some test data
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.x = tf.cast(np.random.random([2, 3, 2]), dtype=tf.float32)
+        self.dot_product_scaled = DotProductAttention(0.2)
+        self.dot_product_unscaled = DotProductAttention(dropout_rate=0.1, num_heads=8, scaled=False)
+
+    # Test that the output shape of the `call` method is the same as the input shape of the queries, keys, and values
+    def test_output_shape(self):
+        queries, keys, values = self.x, self.x, self.x
+        output_scaled = self.dot_product_scaled(queries, keys, values)
+        output_unscaled = self.dot_product_unscaled(queries, keys, values)
+        assert output_scaled.shape == (2, 3, 2)
+        assert output_unscaled.shape == (2, 3, 2)
