c2956c3801755003bed95c8d82df4af087caefc6
Soran Ghaderi <soran.gdr.cs@gmail.com>
2023-03-15 21:35:24 +0000

    doc: AbsolutePositionalEncoding docstrings

--- transformerx/layers/positional_encoding.py
+++ transformerx/layers/positional_encoding.py
@@ -5,60 +5,6 @@ from transformerx.utils import exists
 
 
 # class AbsolutePositionalEncoding(tf.keras.layers.Layer):
-#     """Compute absolute positional encoding object [1]_.
-#
-#     Generate a sinusoid for each dimension of the positional encoding where wavelengths form a geometric progression
-#     from :math:`2π` to :math:`(10000)2π`.
-#
-#     Notes
-#     -----
-#     Absolute Position Encodings are a type of position embeddings for [Transformer-based models] where positional
-#     encodings are added to the input embeddings at the bottoms of the encoder and decoder stacks. The positional
-#     encodings have the same dimension :math:`d_model` as the embeddings, so that the two can be summed. In the original
-#     implementation, sine and cosine functions of different frequencies are used:
-#
-#     .. math::
-#         PE(pos, 2i) = \sin(pos^{2i/d_{model}})
-#
-#         PE(pos, 2i+1) = \cos(pos^{2i/d_{model}})
-#
-#     where  is the position and  is the dimension.
-#
-#
-#     Parameters
-#     ----------
-#     depth :
-#         Embedding Size; Length of the positional encoding's hidden units, the same as the length of Embedding.
-#     dropout_rate :
-#         Float between 0 and 1. Fraction of the input units to drop.
-#     max_len :
-#         Maximum length of the steps to calculate sinusoid
-#
-#     Returns
-#     -------
-#     output:
-#         Tensor of the same shape of the input tensor with positinoal encodings added
-#
-#     Examples
-#     --------
-#     >>> depth, num_steps = 32, 50
-#     >>> pos_encoding = AbsolutePositionalEncoding(depth, dropout_rate=0.2)
-#     >>> x = tf.zeros((1, num_steps, depth))
-#     >>> print(x.shape)
-#     (1, 50, 32)
-#     >>> x = pos_encoding(x, training=False)
-#     >>> P = pos_encoding.P[:, : x.shape[1], :]
-#     >>> print(x.shape)
-#     (1, 50, 32)
-#     >>> print(P.shape)
-#     (1, 50, 32)
-#
-#     References
-#     ----------
-#     .. [1] Vaswani, A., Shazeer, N., Parmar, N., Uszkoreit, J., Jones, L., Gomez, A. N., Kaiser, L., & Polosukhin, I.
-#     (2017). Attention Is All You Need. arXiv. https://doi.org/10.48550/arXiv.1706.03762
-#     """
-#
 #     def __init__(self, depth, dropout_rate=0, max_len=1000):
 #         super(AbsolutePositionalEncoding, self).__init__()
 #         self.dropout = tf.keras.layers.Dropout(dropout_rate)
@@ -82,8 +28,61 @@ from transformerx.utils import exists
 #         X = X + self.P[:, : X.shape[1], :]
 #         return self.dropout(X, **kwargs)
 
-
 class AbsolutePositionalEncoding(tf.keras.layers.Layer):
+    """Compute absolute positional encoding object [1]_.
+
+    Generate a sinusoid for each dimension of the positional encoding where wavelengths form a geometric progression
+    from :math:`2π` to :math:`(10000)2π`.
+
+    Notes
+    -----
+    Absolute Position Encodings are a type of position embeddings for [Transformer-based models] where positional
+    encodings are added to the input embeddings at the bottoms of the encoder and decoder stacks. The positional
+    encodings have the same dimension :math:`d_model` as the embeddings, so that the two can be summed. In the original
+    implementation, sine and cosine functions of different frequencies are used:
+
+    .. math::
+        PE(pos, 2i) = \sin(pos^{2i/d_{model}})
+
+        PE(pos, 2i+1) = \cos(pos^{2i/d_{model}})
+
+    where  is the position and  is the dimension.
+
+
+    Parameters
+    ----------
+    depth :
+        Embedding Size; Length of the positional encoding's hidden units, the same as the length of Embedding.
+    dropout_rate :
+        Float between 0 and 1. Fraction of the input units to drop.
+    max_len :
+        Maximum length of the steps to calculate sinusoid
+
+    Returns
+    -------
+    output:
+        Tensor of the same shape of the input tensor with positinoal encodings added
+
+    Examples
+    --------
+    >>> depth, num_steps = 32, 50
+    >>> pos_encoding = AbsolutePositionalEncoding(depth, dropout_rate=0.2)
+    >>> x = tf.zeros((1, num_steps, depth))
+    >>> print(x.shape)
+    (1, 50, 32)
+    >>> x = pos_encoding(x, training=False)
+    >>> P = pos_encoding.P[:, : x.shape[1], :]
+    >>> print(x.shape)
+    (1, 50, 32)
+    >>> print(P.shape)
+    (1, 50, 32)
+
+    References
+    ----------
+    .. [1] Vaswani, A., Shazeer, N., Parmar, N., Uszkoreit, J., Jones, L., Gomez, A. N., Kaiser, L., & Polosukhin, I.
+    (2017). Attention Is All You Need. arXiv. https://doi.org/10.48550/arXiv.1706.03762
+    """
+
     def __init__(self, depth, dropout_rate=0, max_len=1000):
         super(AbsolutePositionalEncoding, self).__init__()
         self.depth = depth
@@ -101,6 +100,7 @@ class AbsolutePositionalEncoding(tf.keras.layers.Layer):
         X = X + self.P[:, :tf.shape(X)[1], :]
         X = self.dropout(X, **kwargs)
         return X
+
 
 class RelativePositionEmbedding(tf.keras.layers.Layer):
     """Create a relative positional embedding as in [2]_.
