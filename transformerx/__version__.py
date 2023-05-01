20eb4ba6b1ecffb6d792bea2d304e3b8395872eb
Soran Ghaderi <soran.gdr.cs@gmail.com>
2023-05-01 03:11:59 +0100

    fix: __version__.py works properly

--- transformerx/__version__.py
+++ transformerx/__version__.py
@@ -1,5 +1,10 @@
 VERSION = (1, 0, 0, "beta", 3)
 
-__version__ = '.'.join(map(str, VERSION))
+if len(VERSION) < 3:
+    raise ValueError("VERSION must have at least three elements")
 
+__version__ = ".".join(str(v) for v in VERSION[:3])
+if len(VERSION) > 3:
+    __version__ += "-" + ".".join(str(v) for v in VERSION[3:])
+# version_str += "-dev"
 print(__version__)
