bfdbca56f50d4e960678acbd2def911bf91352a0
Soran Ghaderi <soran.gdr.cs@gmail.com>
2023-05-02 21:11:48 +0100

    feat: adding AttentionMask

--- transformerx/layers/masks/base.py
+++ transformerx/layers/masks/base.py
@@ -11,3 +11,13 @@ class BaseMask(tf.keras.layers.Layer):
     def call(self, inputs, *args, **kwargs):
         mask = self.build_mask(inputs.shape)
         return inputs * mask
+
+
+class AttentionMask(BaseMask):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def build_mask(self, input_shape):
+        seq_len = input_shape[1]
+        mask = 1 - tf.linalg.band_part(tf.ones((seq_len, seq_len)), -1, 0)
+        return tf.expand_dims(mask, axis=0)
