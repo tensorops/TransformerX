4ee7744b8b6298815eee8a9646aa9806cc61cb0a
Soran Ghaderi <soran.gdr.cs@gmail.com>
2023-05-27 01:13:02 +0100

    refactor: Modify all maskings
    they seem to work now

--- transformerx/layers/masks/base.py
+++ transformerx/layers/masks/base.py
@@ -9,13 +9,13 @@ class BaseMask(tf.keras.layers.Layer):
         raise NotImplementedError("Subclasses must implement build_mask method")
 
     def call(self, inputs, *args, **kwargs):
-        if len(inputs.shape) == 4:
+        if tf.shape(inputs).shape == 4:
             pass
-        elif len(inputs.shape) == 3:
+        elif tf.shape(inputs).shape == 3:
             inputs = tf.expand_dims(inputs, axis=1)
         else:
             raise f"Invalid input shape. Expected 3D or 4D tensors, but received {len(inputs.shape)}D."
-        mask = self.build_mask()
+        mask = self.build_mask(inputs)
         return tf.add(inputs, mask * -1e9)
 
 
@@ -25,11 +25,12 @@ class LookAheadMask(BaseMask):
 
     def build_mask(self, inputs):
         input_shape = tf.shape(inputs)
-        q_seq_len = input_shape[2]
-        k_seq_len = input_shape[3]
+        if input_shape.shape == 4:
+            print("input shape: ", input_shape)
+            k_seq_len = input_shape[3]
+            q_seq_len = input_shape[2]
+
         mask = 1 - tf.linalg.band_part(tf.ones((q_seq_len, k_seq_len)), -1, 0)
-        mask = tf.expand_dims(mask, axis=1)
-        mask = tf.expand_dims(mask, axis=1)
         return mask
 
 
@@ -37,8 +38,8 @@ class PaddingMask(BaseMask):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    def build_mask(self, input_shape):
-        mask = tf.cast(tf.math.equal(input_shape, 0), tf.float32)
+    def build_mask(self, inputs):
+        mask = tf.cast(tf.math.equal(inputs, 0), tf.float32)
         return mask
 
 
@@ -69,23 +70,17 @@ if __name__ == "__main__":
 
     input_tensor = tf.random.uniform((2, 4, 6))
     q_input_tensor = tf.random.uniform((2, 4, 6))
-    attn_o, attn_w = DotProductAttention()(q_input_tensor, input_tensor, input_tensor)
-    # print("mask attn_o.shape: ", attn_o.shape)
-    # print("mask attn_w.shape:", attn_w.shape)
-    # print("mask attn_w:", attn_w)
-    mask = LookAheadMask()
-    output_tensor = mask(attn_w)
-    # print("masked ouptut shape: ", output_tensor.shape, output_tensor)
-    # print(tf.nn.softmax(output_tensor, axis=-1))
+    attn_o, attn_w = DotProductAttention()(q_input_tensor, q_input_tensor, input_tensor)
 
-    # print(tf.nn.softmax(output_tensor, axis=-1))
+    print("attn_w.shape: ", attn_w.shape)
+    la_mask = LookAheadMask()
+    output_tensor = la_mask(attn_w)
+    print(output_tensor.shape, output_tensor)
 
     multihead_attn = MultiHeadAttention(d_model=32, num_heads=4, dropout_rate=0.1)
     output, attn_w = multihead_attn(q_input_tensor, input_tensor, input_tensor)
-    output_tensor = mask(attn_w)
-    # print("mask output_tensor.shape: ", output_tensor.shape)
-    # print("mask output_tensor.shape: ", attn_w)
-    # print(tf.nn.softmax(output_tensor, axis=-1))
+    output_tensor = la_mask(attn_w)
+    print(output_tensor.shape, output_tensor)
 
     data = [[1, 2, 3], [4, 5], [6, 7, 8, 9]]
     # Create a 2D tensor
@@ -95,12 +90,25 @@ if __name__ == "__main__":
     # data_tensor = tf.constant(data, dtype=tf.float32)
 
     # Create a SequencePadding layer
-    sequence_padding_layer = PaddingLayer(0, 4)
+    # sequence_padding_layer = PaddingLayer(0, 4)
 
-    padded_data = sequence_padding_layer(data)
+    # padded_data = sequence_padding_layer(data)
+
+    # Test input
+    input_tensor = tf.constant(
+        [
+            [[1, 2, 0], [4, 5, 6], [7, 8, 9], [0, 0, 0]],
+            [[1, 2, 3], [4, 5, 0], [0, 0, 0], [0, 0, 0]],
+        ],
+        dtype=tf.float32,
+    )
 
     # Create a PaddingMask layer
     padding_mask_layer = PaddingMask()
 
     # Generate the padding mask
-    padding_mask = padding_mask_layer(padded_data)
+    padding_mask = padding_mask_layer(input_tensor)
+    print(padding_mask.shape, padding_mask)
+
+    lad_mask = la_mask(input_tensor)
+    print(lad_mask.shape, lad_mask)
