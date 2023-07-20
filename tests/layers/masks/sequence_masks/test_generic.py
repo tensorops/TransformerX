e1817b11d17351767bc7e7c5b8798170cb385fba
Soran Ghaderi <soran.gdr.cs@gmail.com>
2023-07-20 23:48:11 +0100

    test: test_valid_lengths

--- tests/layers/masks/sequence_masks/test_generic.py
+++ tests/layers/masks/sequence_masks/test_generic.py
@@ -6,4 +6,10 @@ from transformerx.layers.masks.core import BaseMask
 
 
 class TestPaddingMask:
-    pass
+    def test_valid_lengths(self):
+        scores = tf.constant([[1, 2, 3, 0], [4, 5, 0, 0]], dtype=tf.float32)
+        valid_lens = tf.constant([3, 2])
+        padding_mask = PaddingMask()
+        masked = padding_mask(scores=scores, valid_lens=valid_lens)
+        expected = tf.constant([[1, 2, 3, -1e9], [4, 5, -1e9, -1e9]])
+        assert tf.reduce_all(tf.equal(masked, expected))
