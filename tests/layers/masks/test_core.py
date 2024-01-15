test: Lookahead works fine and its test case is correct the multihead for the lookahead works fine but the multihead is not affected yet.

tests/layers/masks/test_core.py
@@ -8,30 +8,41 @@ def test_lookahead_mask():
     attn_o, attn_w = DotProductAttention()(
         q_input_tensor, q_input_tensor, q_input_tensor
     )
-
+    mask_value = -1e9
     la_mask = LookAheadMask()
     output_tensor = la_mask(attn_w)
 
+    # Create a test mask manually using tf.linalg.band_part: this creates upper triangular boolean matrix which then
+    # will be multiplied by the mask_value which is 10-9, and then we add this new mask matrix which its upper triangle
+    # values are 10-9 with the scores (attention) matrix. Later when we pass this matrix to the softmax, they will
+    # become 0 since they are -inf numbers.
+    test_mask = 1 - tf.linalg.band_part(tf.ones_like(attn_w), -1, 0)
+    test_mask = mask_value * tf.cast(test_mask, dtype=attn_w.dtype)
+    test_masked_attn = tf.add(attn_w, test_mask)
+
     assert output_tensor.shape == attn_w.shape
+    tf.debugging.assert_equal(output_tensor, test_masked_attn,
+                              message="Mismatch in tensors")
     assert tf.reduce_all(
-        output_tensor == tf.linalg.band_part(tf.ones_like(attn_w), -1, 0)
+        output_tensor == test_masked_attn
     )
 
 
 def test_multihead_attention():
-    q_input_tensor = tf.random.uniform((2, 4, 6))
-    input_tensor = tf.random.uniform((2, 4, 6))
-    multihead_attn = MultiHeadAttention(d_model=32, num_heads=4, dropout_rate=0.1)
+    d_model = 256
+    q_input_tensor = tf.random.uniform((2, 4, d_model))
+    input_tensor = tf.random.uniform((2, 4, d_model))
+    multihead_attn = MultiHeadAttention(d_model=d_model, num_heads=4, dropout_rate=0.1, causal_mask=True)
     output, attn_w = multihead_attn(q_input_tensor, input_tensor, input_tensor)
 
-    assert output.shape == q_input_tensor.shape
-    assert attn_w.shape == (2, 4, 4)
+    assert output.shape == q_input_tensor.shape  # (batch_size, sequence_length, d_model)
+    assert attn_w.shape == (2, 4, 4, 4)  # (batch_size, num_heads, sequence_length, sequence_length)
 
 
 def test_padding_mask():
     inputs = tf.random.normal(
         (2, 3, 4)
-    )  # 3D input tensor (batch_size=2, query_len=3, key_dim=4)
+    )  # 3D input tensor (batch_size=2, q_len=3, k_len=4)
     valid_lens = tf.constant(
         [2, 3], dtype=tf.int32
     )  # Valid lengths for each sequence in the batch

transformerx/layers/dot_product_attention.py
@@ -139,12 +139,12 @@ class DotProductAttention(tf.keras.layers.Layer):
         # apply causal mask
         if self.causal_mask:
             # todo: get different masks as a single or list of Callable or str objects and then invoke them in a loop
+            # todo: for performance reasons, first generate the boolean masks and then in the end add up them and then
+            #  multiply them once instead of generating masks and then multiply with 10-9 and add again etc.
 
             # New version of masking
             look_ahead_mask = LookAheadMask()
-            print("raw scores: ", scores)
             scores = look_ahead_mask(scores)
-            print("causal masked scores: ", scores)
 
             # todo: now add the newly implemented padding mask here
             padding_mask = PaddingMask()

transformerx/layers/masks/sequence_masks/generic.py
@@ -12,6 +12,10 @@ from transformerx.layers.masks.core import BaseMask
 
 class LookAheadMask(BaseMask):
     def build_mask(self, q_len, k_len, *args, **kwargs):
+        # Create a test mask manually using tf.linalg.band_part: this creates upper triangular boolean matrix which then
+        # will be multiplied by the mask_value which is 10-9, and then we add this new mask matrix which its upper
+        # triangle values are 10-9 with the scores (attention) matrix. Later when we pass this matrix to the softmax,
+        # they will become 0 since they are -inf numbers.
         mask = (
             1
             - tf.linalg.LinearOperatorLowerTriangular(
@@ -19,6 +23,28 @@ class LookAheadMask(BaseMask):
             ).to_dense()
         )
         return mask
+
+# class LookAheadMask(tf.keras.layers.Layer):
+#     def __init__(self, **kwargs):
+#         super(LookAheadMask, self).__init__(**kwargs)
+#
+#     def build(self, input_shape):
+#         super(LookAheadMask, self).build(input_shape)
+#
+#     def call(self, inputs, **kwargs):
+#         sequence_length = tf.shape(inputs)[1]
+#
+#         # Create a lower triangular matrix with ones
+#         mask = tf.linalg.band_part(tf.ones((sequence_length, sequence_length)), -1, 0)
+#
+#         # Expand the mask to the batch dimension
+#         mask = tf.expand_dims(mask, 0)
+#         mask = tf.tile(mask, [tf.shape(inputs)[0], 1, 1])
+#
+#         return mask
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
 
 
 """Future features for PaddingMask:
@@ -58,7 +84,7 @@ class PaddingMask(BaseMask):
             #     dtype=self.scores_dtype,
             # )
 
-            mask = tf.cast(padding_mask, dtype=self.scores_dtype)
+            padding_mask = tf.cast(padding_mask, dtype=self.scores_dtype)
             mask = padding_mask
             print("result padding mask: ", mask)
         elif valid_lens is not None:
@@ -66,6 +92,7 @@ class PaddingMask(BaseMask):
             # max_len = tf.reduce_max(valid_lens)
             print("here", self.padding_value)
             mask = 1 - tf.sequence_mask(valid_lens, max_len, dtype=self.scores_dtype)
+            print("mask: ", mask)
 
         elif scores is not None:
             mask = tf.cast(